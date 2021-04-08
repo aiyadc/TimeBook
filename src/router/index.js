08868cfd767ddff6212032985804d23b346e8bb5
aiyadc <565548447@qq.com>
2021-04-09 00:24:29 +0800

    配置jwt

--- src/router/index.js
+++ src/router/index.js
@@ -5,55 +5,59 @@ import store from "../store";
 Vue.use(Router);
 
 const router = new Router({
-  routes: [
-    {
-      path: "/",
-      name: "Home",
-      component: () => import("@/views/template/index.vue")
-    },
-    {
-      path: "/diy/:aid",
-      name: "diy",
-      meta: {
-        title: "相册DIY"
-      },
-      component: () => import("@/views/DIY/index")
-    },
-    {
-      path: "/login",
-      component: () => import("@/views/login")
-    },
-    {
-      path: "/album",
-      component: () => import("@/test/cameras-and-albums")
-    },
-    {
-      path: "/mine",
-      component: () => import("@/views/mine/index.vue")
-    }
-  ]
-});
-router.beforeEach((to, from, next) => {
-  const publicURL = ["/login"];
-  console.log("store.state.uid :>> ", store.state.uid);
-  if (!publicURL.includes(to.path)) {
-    if (store.state.uid == 0) {
-      next("/login");
-    } else {
-      next();
-    }
-  } else {
-    next();
-  }
+    routes: [{
+            path: "/",
+            name: "Home",
+            component: () =>
+                import ("@/views/template/index.vue")
+        },
+        {
+            path: "/diy/:aid",
+            name: "diy",
+            meta: {
+                title: "相册DIY"
+            },
+            component: () =>
+                import ("@/views/DIY/index")
+        },
+        {
+            path: "/login",
+            component: () =>
+                import ("@/views/login")
+        },
+        {
+            path: "/album",
+            component: () =>
+                import ("@/test/cameras-and-albums")
+        },
+        {
+            path: "/mine",
+            component: () =>
+                import ("@/views/mine/index.vue")
+        }
+    ]
 });
 // router.beforeEach((to, from, next) => {
-//     const publicURL = ["/login"];
-//     console.log("store.state.uid :>> ", store.state.uid);
-//     if (!publicURL.includes(to.path)) {
-
+//   const publicURL = ["/login"];
+//   console.log("store.state.uid :>> ", store.state.uid);
+//   if (!publicURL.includes(to.path)) {
+//     if (store.state.uid == 0) {
+//       next("/login");
 //     } else {
-//         next();
+//       next();
 //     }
+//   } else {
+//     next();
+//   }
 // });
+router.beforeEach((to, from, next) => {
+    const publicURL = ["/login"];
+    console.log("store.state.uid :>> ", store.state.uid);
+    if (!publicURL.includes(to.path)) {
+
+    } else {
+        next();
+    }
+});
 
 export default router;
